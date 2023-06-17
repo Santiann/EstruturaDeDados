6011e2b52a9c7823e09e155730c76a697f39f125
Santiann <joaosantian@hotmail.com>
2023-06-17 16:26:56 -0300

    Ajustes

--- main.c
+++ main.c
@@ -1,189 +1,299 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
 
-RBNode* createRBNode(Artist artist) {
-    RBNode* newNode = (RBNode*)malloc(sizeof(RBNode));
-    newNode->artist = artist;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    newNode->parent = NULL;
-    newNode->color = 1; // Red by default
-    return newNode;
+#define MAX_LINE_LENGTH 1000
+#define MAX_ACTORS 100
+#define MAX_MOVIES 100
+
+typedef struct {
+    int id;
+    char primaryName[MAX_LINE_LENGTH];
+    char knownForTitles[MAX_LINE_LENGTH];
+} Actor;
+
+typedef struct {
+    int id;
+    char primaryTitle[MAX_LINE_LENGTH];
+} Movie;
+
+typedef struct {
+    int numActors;
+    Actor actors[MAX_ACTORS];
+} ActorTree;
+
+typedef struct Node {
+    Actor actor;
+    struct Node* parent;
+    struct Node* left;
+    struct Node* right;
+    int color; // 0 for black, 1 for red
+} Node;
+
+typedef struct {
+    int numMovies;
+    Movie movies[MAX_MOVIES];
+    int adjacencyMatrix[MAX_MOVIES][MAX_MOVIES];
+} MovieGraph;
+
+Node* createNode(Actor actor) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->actor = actor;
+    node->left = NULL;
+    node->right = NULL;
+    node->color = 1; // New node is always red
+    return node;
 }
 
-void rbInsert(RBNode** root, RBNode* newNode) {
-    // Perform a standard BST insertion
-    RBNode* parent = NULL;
-    RBNode* current = *root;
+Node* bstInsert(Node* root, Node* newNode) {
+    if (root == NULL)
+        return newNode;
 
-    while (current != NULL) {
-        parent = current;
-        if (newNode->artist.id < current->artist.id)
-            current = current->left;
-        else
-            current = current->right;
-    }
+    if (newNode->actor.id < root->actor.id)
+        root->left = bstInsert(root->left, newNode);
+    else if (newNode->actor.id > root->actor.id)
+        root->right = bstInsert(root->right, newNode);
 
-    newNode->parent = parent;
+    return root;
+}
 
-    if (parent == NULL)
-        *root = newNode;
-    else if (newNode->artist.id < parent->artist.id)
-        parent->left = newNode;
+void leftRotate(Node** root, Node* node) {
+    Node* rightChild = node->right;
+    node->right = rightChild->left;
+
+    if (node->right != NULL)
+        node->right->color = 1;
+
+    rightChild->color = node->color;
+    rightChild->left = node;
+    node->color = 0;
+
+    if (node == *root)
+        *root = rightChild;
+    else if (node == node->left)
+        node->left = rightChild;
     else
-        parent->right = newNode;
-
+        node->right = rightChild;
 }
 
-AdjListNode* createAdjListNode(int movieId) {
-    AdjListNode* newNode = (AdjListNode*)malloc(sizeof(AdjListNode));
-    newNode->movieId = movieId;
-    newNode->next = NULL;
-    return newNode;
+void rightRotate(Node** root, Node* node) {
+    Node* leftChild = node->left;
+    node->left = leftChild->right;
+
+    if (node->left != NULL)
+        node->left->color = 1;
+
+    leftChild->color = node->color;
+    leftChild->right = node;
+    node->color = 0;
+
+    if (node == *root)
+        *root = leftChild;
+    else if (node == node->right)
+        node->right = leftChild;
+    else
+        node->left = leftChild;
 }
 
-Graph* createGraph(int numMovies) {
-    Graph* graph = (Graph*)malloc(sizeof(Graph));
-    graph->numMovies = numMovies;
-    graph->array = (AdjList*)malloc(numMovies * sizeof(AdjList));
-    for (int i = 0; i < numMovies; i++) {
-        graph->array[i].head = NULL;
+void flipColors(Node* node) {
+    node->color = 1;
+    node->left->color = 0;
+    node->right->color = 0;
+}
+
+void fixRedRed(Node** root, Node* node) {
+    if (node == *root) {
+        node->color = 0;
+        return;
     }
-    return graph;
-}
 
-void addEdge(Graph* graph, int src, int dest) {
-    AdjListNode* newNode = createAdjListNode(dest);
-    newNode->next = graph->array[src].head;
-    graph->array[src].head = newNode;
-}
+    Node* parent = NULL;
+    Node* grandparent = NULL;
+    Node* greatGrandparent = NULL;
 
-void printGraph(Graph* graph) {
-    printf("graph {\n");
-    for (int i = 0; i < graph->numMovies; i++) {
-        AdjListNode* node = graph->array[i].head;
-        while (node != NULL) {
-            printf(" \"%s\" -- \"%s\"\n", graph->array[i].title, graph->array[node->movieId].title);
-            node = node->next;
+    while (node != *root && node->color == 1 && node->parent->color == 1) {
+        parent = node->parent;
+        grandparent = parent->parent;
+
+        if (parent == grandparent->left) {
+            greatGrandparent = grandparent->parent;
+
+            Node* uncle = grandparent->right;
+            if (uncle != NULL && uncle->color == 1) {
+                grandparent->color = 1;
+                parent->color = 0;
+                uncle->color = 0;
+                node = grandparent;
+            }
+            else {
+                if (node == parent->right) {
+                    leftRotate(root, parent);
+                    node = parent;
+                    parent = node->parent;
+                }
+                rightRotate(root, grandparent);
+                int tempColor = parent->color;
+                parent->color = grandparent->color;
+                grandparent->color = tempColor;
+                node = parent;
+            }
+        }
+        else {
+            greatGrandparent = grandparent->parent;
+
+            Node* uncle = grandparent->left;
+            if (uncle != NULL && uncle->color == 1) {
+                grandparent->color = 1;
+                parent->color = 0;
+                uncle->color = 0;
+                node = grandparent;
+            }
+            else {
+                if (node == parent->left) {
+                    rightRotate(root, parent);
+                    node = parent;
+                    parent = node->parent;
+                }
+                leftRotate(root, grandparent);
+                int tempColor = parent->color;
+                parent->color = grandparent->color;
+                grandparent->color = tempColor;
+                node = parent;
+            }
         }
     }
+
+    (*root)->color = 0;
+}
+
+void bstInsertWithFix(Node** root, Node* newNode) {
+    *root = bstInsert(*root, newNode);
+    fixRedRed(root, newNode);
+}
+
+void readActorsFile(ActorTree* actorTree, const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Failed to open file: %s\n", filename);
+        exit(1);
+    }
+
+    char line[MAX_LINE_LENGTH];
+
+    // Skip header line
+    fgets(line, sizeof(line), file);
+
+    int actorCount = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL && actorCount < MAX_ACTORS) {
+        char* token = strtok(line, "\t");
+
+        Actor actor;
+        actor.id = atoi(token + 2); // Skip the "nm" prefix
+        strcpy(actor.primaryName, strtok(NULL, "\t"));
+        strcpy(actor.knownForTitles, strtok(NULL, "\t"));
+
+        actorTree->actors[actorCount++] = actor;
+    }
+
+    actorTree->numActors = actorCount;
+
+    fclose(file);
+}
+
+void readMoviesFile(MovieGraph* movieGraph, const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Failed to open file: %s\n", filename);
+        exit(1);
+    }
+
+    char line[MAX_LINE_LENGTH];
+
+    // Skip header line
+    fgets(line, sizeof(line), file);
+
+    int movieCount = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL && movieCount < MAX_MOVIES) {
+        char* token = strtok(line, "\t");
+
+        if (strcmp(token + 2, "movie") != 0)
+            continue; // Skip non-movie entries
+
+        Movie movie;
+        movie.id = atoi(token + 2);
+        strcpy(movie.primaryTitle, strtok(NULL, "\t"));
+
+        movieGraph->movies[movieCount++] = movie;
+    }
+
+    movieGraph->numMovies = movieCount;
+
+    fclose(file);
+}
+
+int findMovieIndex(MovieGraph* movieGraph, int movieId) {
+    for (int i = 0; i < movieGraph->numMovies; i++) {
+        if (movieGraph->movies[i].id == movieId)
+            return i;
+    }
+    return -1;
+}
+
+void addEdge(MovieGraph* movieGraph, int movieIndex1, int movieIndex2) {
+    movieGraph->adjacencyMatrix[movieIndex1][movieIndex2] = 1;
+    movieGraph->adjacencyMatrix[movieIndex2][movieIndex1] = 1;
+}
+
+void buildMovieGraph(MovieGraph* movieGraph, ActorTree* actorTree) {
+    for (int i = 0; i < actorTree->numActors; i++) {
+        Actor actor = actorTree->actors[i];
+        char* token = strtok(actor.knownForTitles, ",");
+        while (token != NULL) {
+            int movieId = atoi(token + 2); // Skip the "tt" prefix
+            int movieIndex = findMovieIndex(movieGraph, movieId);
+            if (movieIndex != -1) {
+                for (int j = 0; j < movieGraph->numMovies; j++) {
+                    if (j != movieIndex && movieGraph->adjacencyMatrix[movieIndex][j] == 1) {
+                        addEdge(movieGraph, j, movieIndex);
+                    }
+                }
+            }
+            token = strtok(NULL, ",");
+        }
+    }
+}
+
+void printGraphDot(MovieGraph* movieGraph) {
+    printf("graph {\n");
+
+    for (int i = 0; i < movieGraph->numMovies; i++) {
+        for (int j = i + 1; j < movieGraph->numMovies; j++) {
+            if (movieGraph->adjacencyMatrix[i][j] == 1) {
+                printf("\t\"%s\" -- \"%s\";\n", movieGraph->movies[i].primaryTitle, movieGraph->movies[j].primaryTitle);
+            }
+        }
+    }
+
     printf("}\n");
 }
 
-void freeGraph(Graph* graph) {
-    if (graph == NULL) return;
-    for (int i = 0; i < graph->numMovies; i++) {
-        AdjListNode* node = graph->array[i].head;
-        while (node != NULL) {
-            AdjListNode* temp = node;
-            node = node->next;
-            free(temp);
-        }
-    }
-    free(graph->array);
-    free(graph);
-}
-
-void buildArtistTree(char* filename, RBNode** root) {
-
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Erro ao abrir o arquivo %s\n", filename);
-        return;
-    }
-
-    char line[1000];
-    int lineCount = 0;
-    fgets(line, sizeof(line), file); // Ignora o cabeçalho
-    while (fgets(line, sizeof(line), file) && lineCount < 100) {
-        Artist artist;
-        sscanf(line, "%d %[^\t\n]s", &artist.id, artist.name);
-        
-        // Ler a lista de filmes e atribuí-la ao artista
-        char* token = strtok(line, "\t"); // Pular o ID do artista
-        token = strtok(NULL, "\t"); // Pular o nome do artista
-        token = strtok(NULL, "\t"); // Pular o ano de nascimento
-        token = strtok(NULL, "\t"); // Pular o ano de falecimento
-        token = strtok(NULL, "\t"); // Pular a lista de profissões
-        
-        // Obter a lista de filmes do artista
-        char* movieToken = strtok(token, ",");
-        artist.numMovies = 0;
-        while (movieToken != NULL) {
-            artist.numMovies++;
-            movieToken = strtok(NULL, ",");
-        }
-        artist.movies = (int*)malloc(artist.numMovies * sizeof(int));
-        
-        movieToken = strtok(token, ",");
-        int i = 0;
-        while (movieToken != NULL) {
-            sscanf(movieToken, "tt%d", &artist.movies[i]);
-            i++;
-            movieToken = strtok(NULL, ",");
-        }
-
-        // Inserir o artista na árvore
-        RBNode* newNode = createRBNode(artist);
-        rbInsert(root, newNode);
-        lineCount++;
-    }
-
-    fclose(file);
-}
-
-void buildMovieGraph(char* filename, Graph** graph) {
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("teste");
-        printf("Erro ao abrir o arquivo %s\n", filename);
-        return;
-    }
-
-    char line[1000];
-    fgets(line, sizeof(line), file); // Ignora o cabeçalho
-    int numMovies = 0;
-    int lineCount = 0;
-    while (fgets(line, sizeof(line), file) && lineCount < 100) {
-        char type[100], title[100];
-        sscanf(line, "%*s %[^\t\n]s\t%[^\t\n]s", type, title);
-        if (strcmp(type, "movie") == 0) {
-            numMovies++;
-        }
-
-        lineCount++;
-    }
-    *graph = createGraph(numMovies);
-
-    rewind(file); // Voltar ao início do arquivo
-
-    fgets(line, sizeof(line), file); // Ignora o cabeçalho
-    int movieIndex = 0;
-    while (fgets(line, sizeof(line), file)) {
-        char type[100], title[100];
-        sscanf(line, "%*s %[^\t\n]s\t%[^\t\n]s", type, title);
-        if (strcmp(type, "movie") == 0) {
-            strcpy((*graph)->array[movieIndex].title, title);
-            movieIndex++;
-        }
-    }
-
-    fclose(file);
-}
-
 int main() {
-    RBNode* artistTree = NULL;
-    Graph* movieGraph = NULL;
+    ActorTree actorTree;
+    readActorsFile(&actorTree, "C:/Versionamento/Estrutura/TrabalhoFinal/EstruturaDeDados/name.basics.tsv");
 
-    buildArtistTree("C:/Versionamento/Estrutura/dados/name.basics.tsv", &artistTree);
-    buildMovieGraph("C:/Versionamento/Estrutura/dados/title.basics.tsv", &movieGraph);
+    Node* root = NULL;
+    for (int i = 0; i < actorTree.numActors; i++) {
+        Node* newNode = createNode(actorTree.actors[i]);
+        bstInsertWithFix(&root, newNode);
+    }
 
-    // TODO: Percorrer a árvore e criar as arestas do grafo
+    MovieGraph movieGraph;
+    readMoviesFile(&movieGraph, "C:/Versionamento/Estrutura/TrabalhoFinal/EstruturaDeDados/title.basics.tsv");
+    buildMovieGraph(&movieGraph, &actorTree);
 
-    printGraph(movieGraph);
-    freeGraph(movieGraph);
+    printGraphDot(&movieGraph);
 
     return 0;
 }
